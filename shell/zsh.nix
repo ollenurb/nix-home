8235264ed9f6c108e0fb72bd16b90730a6d71f0d
Ollenurb <brunello.mtt@gmail.com>
2021-10-02 14:54:27 +0200

    Added nix-shell zsh plugin

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -3,6 +3,7 @@
 {
   programs.zsh = {
     enable = true;
+    # Setup custom aliases
     shellAliases = {
       apply-system = "$HOME/.dotfiles/apply-system.sh";
       apply-home = "$HOME/.dotfiles/apply-home.sh";
@@ -11,8 +12,23 @@
       ls = "ls --color=auto";
       l = "exa -l --icons";
     };
+    # Setup custom prompt
     initExtra = ''
         PROMPT="%B%F{blue}λ%f %F{244}%f %F{cyan}%1~%f%b "
     '';
+    # Add plugins
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.4.0";
+          sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+        };
+      }
+    ];
+
   };
 }
